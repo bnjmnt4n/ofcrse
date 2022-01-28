e9a0c8126aeed7364c442577a084bf2d14045ca4
Benjamin Tan <benjamin@dev.ofcr.se>
2022-01-28 23:26:27 +0800

    Tweak Nix flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs?rev=c07b471b52be8fbc49a7dc194e9b37a6e19ee04d";
+    flake-utils.url = "github:numtide/flake-utils?rev=846b2ae0fc4cc943637d3d1def4454213e203cba";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
@@ -9,10 +10,9 @@
         pkgs = nixpkgs.legacyPackages."${system}";
       in {
        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
+          buildInputs = with pkgs; [
+            flyctl
             nodejs-16_x
-            nodePackages.eslint_d
-            nodePackages.typescript-language-server
           ];
         };
       });
