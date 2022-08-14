28ba01c47e2250bd005ec82192e347988d3303c4
Benjamin Tan <benjamin@dev.ofcr.se>
2022-08-14 18:01:31 +0800

    Bump dependencies

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?rev=38860c9e91cb00f4d8cd19c7b4e36c45680c89b5";
+    nixpkgs.url = "github:NixOS/nixpkgs?rev=12363fb6d89859a37cd7e27f85288599f13e49d9";
     flake-utils.url = "github:numtide/flake-utils?rev=7e2a3b3dfd9af950a856d66b0a7d01e3c18aa249";
   };
 
