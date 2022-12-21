522d430e9fc668d7a205a1da7e86cfb03608a1bd
Benjamin Tan <benjamin@dev.ofcr.se>
2022-12-21 16:16:56 +0800

    Flake: bump dependencies and setup Rust tools

--- flake.nix
+++ flake.nix
@@ -1,19 +1,65 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?rev=12363fb6d89859a37cd7e27f85288599f13e49d9";
-    flake-utils.url = "github:numtide/flake-utils?rev=7e2a3b3dfd9af950a856d66b0a7d01e3c18aa249";
+    nixpkgs.url = "github:NixOS/nixpkgs?rev=04f574a1c0fde90b51bf68198e2297ca4e7cccf4";
+    flake-utils.url = "github:numtide/flake-utils?rev=5aed5285a952e0b949eb3ba02c12fa4fcfef535f";
+    naersk.url = "github:nmattia/naersk";
+    mozillapkgs = {
+      url = "github:mozilla/nixpkgs-mozilla";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, naersk, mozillapkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages."${system}";
-      in {
-       devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            flyctl
-            nodejs-18_x
-          ];
+
+        mozilla = pkgs.callPackage (mozillapkgs + "/package-set.nix") { };
+        rust-channel = mozilla.rustChannelOf {
+          channel = "1.66.0";
+          sha256 = "sha256-S7epLlflwt0d1GZP44u5Xosgf6dRrmr8xxC+Ml2Pq7c=";
+        };
+        rust = rust-channel.rust;
+        rust-src = rust-channel.rust-src;
+
+        naersk-lib = naersk.lib."${system}".override {
+          cargo = rust;
+          rustc = rust;
+        };
+
+        nativeBuildInputs = [
+          pkgs.openssl
+          pkgs.pkg-config
+        ];
+        buildInputs = [ ];
+      in
+      rec {
+        packages.package = naersk-lib.buildPackage {
+          pname = "ofcr.se";
+          root = ./.;
+          inherit nativeBuildInputs buildInputs;
+        };
+        defaultPackage = packages.package;
+
+        apps.package = flake-utils.lib.mkApp {
+          drv = packages.package;
+        };
+        defaultApp = apps.package;
+
+        devShell = pkgs.mkShell {
+          inherit nativeBuildInputs;
+          buildInputs = [
+            pkgs.flyctl
+            pkgs.nodejs-18_x
+
+            rust
+            pkgs.rust-analyzer
+            pkgs.rustfmt
+          ] ++ buildInputs;
+          RUST_SRC_PATH = "${rust-src}/lib/rustlib/src/rust/library";
+          RUST_LOG = "info";
+          RUST_BACKTRACE = 1;
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
         };
       });
 }
