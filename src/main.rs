18db494b197966c3e4547dc018bf7d1f1cbcbbf5
Benjamin Tan <benjamin@dev.ofcr.se>
2022-12-21 16:36:16 +0800

    Add basic `axum` app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+use axum::{routing::get, Router};
+
+const DEFAULT_PORT: i32 = 3000;
+
+#[tokio::main]
+async fn main() {
+    let port: u16 = std::env::var("PORT")
+        .unwrap_or(DEFAULT_PORT.to_string())
+        .parse()
+        .unwrap();
+    let address = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), port);
+
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    axum::Server::bind(&address)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
