f683f03afc7240c40da8cff4aaf54d16ca60266d
Benjamin Tan <benjamin@dev.ofcr.se>
2022-12-21 17:41:45 +0800

    Setup hostname-based routing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
-use axum::{routing::get, Router};
+use axum::{
+    body::Body,
+    extract::Host,
+    http::{Request, StatusCode},
+    response::IntoResponse,
+    routing::{any, get, get_service},
+    Router,
+};
+use tower::util::ServiceExt;
+use tower_http::services::{ServeDir, ServeFile};
 
 const DEFAULT_PORT: i32 = 3000;
 
@@ -12,10 +21,30 @@ async fn main() {
         .unwrap();
     let address = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), port);
 
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/", any(handler))
+        .route("/*path", any(handler));
 
     axum::Server::bind(&address)
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn handler(Host(hostname): Host, request: Request<Body>) -> impl IntoResponse {
+    let file_server = ServeDir::new("dist").not_found_service(ServeFile::new("dist/404.html"));
+    let primary_app =
+        Router::new().fallback_service(get_service(file_server).handle_error(handle_error));
+
+    let health_check_app = Router::new().route("/health_check", get(|| async { "ok" }));
+
+    match hostname.as_str() {
+        "health.check" => health_check_app.oneshot(request),
+        _ => primary_app.oneshot(request),
+    }
+    .await
+}
+
+async fn handle_error(_err: std::io::Error) -> impl IntoResponse {
+    (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong...")
 }
