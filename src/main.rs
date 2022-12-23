64040822c0d5565c723b0f0db10dffc5a1773014
Benjamin Tan <benjamin@dev.ofcr.se>
2022-12-23 15:18:34 +0800

    Add GoatCounter proxy and HTTP-to-HTTPS redirects

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
-use std::{collections::HashMap, io::BufReader, net::SocketAddr};
+use std::collections::HashMap;
+use std::io::BufReader;
+use std::net::SocketAddr;
 
 use axum::{
     body::Body,
-    extract::{Host, Path},
-    http::{header, Request, StatusCode},
-    response::{IntoResponse, Redirect},
+    extract::{FromRef, Host, Path, State},
+    http::uri::Scheme,
+    http::{header, uri::Authority, Request, StatusCode},
+    middleware::{self, Next},
+    response::{IntoResponse, Response},
     routing::{any, get, get_service},
     Router,
 };
-use tower::util::ServiceExt;
+use hyper::{client::HttpConnector, Uri};
+use tower::ServiceExt;
 use tower_http::{
     services::{ServeDir, ServeFile},
     trace::TraceLayer,
@@ -22,6 +27,8 @@ const DEFAULT_SHORTLINKS_DB: &str = "shortlinks.json";
 #[derive(Clone)]
 struct AppState {
     site_url: String,
+    goatcounter_url: String,
+    goatcounter_host: String,
     shortlinks: HashMap<String, String>,
 }
 
@@ -42,11 +49,22 @@ async fn main() {
     let address = SocketAddr::from(([0, 0, 0, 0], port));
 
     let site_url = std::env::var("SITE_URL").unwrap_or(DEFAULT_SITE_URL.to_string());
+
+    let goatcounter_url = std::env::var("GOATCOUNTER_URL").unwrap_or(DEFAULT_SITE_URL.to_string());
+    let goatcounter_host = Uri::try_from(&goatcounter_url)
+        .unwrap()
+        .host()
+        .unwrap()
+        .to_string();
+
     let shortlinks_database =
         std::env::var("SHORTLINKS_DB").unwrap_or(DEFAULT_SHORTLINKS_DB.to_string());
     let shortlinks = read_shortlinks_from_file(shortlinks_database).unwrap();
+
     let app_state = AppState {
         site_url,
+        goatcounter_url,
+        goatcounter_host,
         shortlinks,
     };
 
@@ -54,10 +72,8 @@ async fn main() {
     // Required for both the standalone `/` path and the wildcard path.
     let app = initialize_app(app, app_state.clone(), "/");
     let app = initialize_app(app, app_state, "/*path");
-    let app = app.fallback_service(
-        // TODO: serve with 404 status code.
-        get_service(ServeFile::new("dist/404.html")).handle_error(handle_error),
-    );
+    // TODO: return 404 file.
+    let app = app.fallback(|| async { (StatusCode::NOT_FOUND, "404 Not Found") });
     let app = app.layer(TraceLayer::new_for_http());
 
     axum::Server::bind(&address)
@@ -67,14 +83,17 @@ async fn main() {
 }
 
 fn initialize_app(app: Router, app_state: AppState, path: &str) -> Router {
-    let primary_app = primary_router();
+    let primary_app = primary_router().with_state(PrimaryAppState {
+        client: Client::new(),
+        goatcounter: (app_state.goatcounter_url, app_state.goatcounter_host),
+    });
 
     let music_shortlink = app_state.shortlinks.get("music").unwrap().clone();
-    let music_shortlink_app = music_shortlink_router(music_shortlink);
+    let music_shortlink_app = music_shortlink_router(music_shortlink).with_state(());
 
-    let shortlink_app = shortlinks_router(app_state.site_url, app_state.shortlinks);
+    let shortlink_app = shortlinks_router(app_state.site_url, app_state.shortlinks).with_state(());
 
-    let health_check_app = health_check_router();
+    let health_check_app = health_check_router().with_state(());
 
     app.route(
         path,
@@ -90,11 +109,116 @@ fn initialize_app(app: Router, app_state: AppState, path: &str) -> Router {
     )
 }
 
+type Client = hyper::client::Client<HttpConnector, Body>;
+
+#[derive(Clone, FromRef)]
+struct PrimaryAppState {
+    client: Client,
+    goatcounter: (String, String),
+}
+
 // Primary static file server.
-fn primary_router() -> Router {
-    // Primary static file server.
+fn primary_router() -> Router<PrimaryAppState> {
     let file_server = ServeDir::new("dist").not_found_service(ServeFile::new("dist/404.html"));
-    Router::new().fallback_service(get_service(file_server).handle_error(handle_error))
+    Router::new()
+        .route("/count", any(goatcounter_proxy))
+        .route("/count/", any(goatcounter_proxy))
+        .route("/count/*path", any(goatcounter_proxy))
+        .fallback_service(get_service(file_server).handle_error(handle_error))
+        .layer(middleware::from_fn(redirect_to_https))
+}
+
+#[axum::debug_handler(state = PrimaryAppState)]
+async fn goatcounter_proxy(
+    State(client): State<Client>,
+    State((goatcounter_url, goatcounter_host)): State<(String, String)>,
+    mut req: Request<Body>,
+) -> Result<Response<Body>, (StatusCode, String)> {
+    let path = req
+        .uri()
+        .path_and_query()
+        .map(|v| v.as_str())
+        .unwrap_or_else(|| req.uri().path());
+    let path = path.strip_prefix("/count").unwrap();
+
+    let uri = format!("{}{}", goatcounter_url, path);
+    *req.uri_mut() = Uri::try_from(uri).map_err(|err| {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Could not construct proxied URL: {:?}", err),
+        )
+    })?;
+
+    let headers_map = req.headers_mut();
+    // Remove any unnecessary headers.
+    headers_map.remove("fly-forwarded-port");
+    headers_map.remove("fly-region");
+    headers_map.remove("x-forwarded-proto");
+    headers_map.remove("x-forwarded-port");
+    headers_map.remove("x-forwarded-ssl");
+    // `X-Forwarded-For` header will be forwarded.
+    headers_map.insert("Host", goatcounter_host.parse().unwrap());
+    headers_map
+        .remove("fly-client-ip")
+        .and_then(|real_ip| headers_map.insert("X-Real-IP", real_ip));
+
+    Ok(client.request(req).await.map_err(|err| {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Error proxying request: {:?}", err),
+        )
+    })?)
+}
+
+// Redirect any non-HTTPS requests to HTTP.
+async fn redirect_to_https<B>(
+    Host(hostname): Host,
+    req: Request<B>,
+    next: Next<B>,
+) -> Result<Response, (StatusCode, String)> {
+    let proto: &str = req
+        .headers()
+        .get("x-forwarded-proto")
+        .map(|header| header.to_str())
+        .unwrap_or(Ok("https"))
+        .map_err(|err| {
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!(
+                    "Could not convert X-Forwarded-Proto header to string: {:?}",
+                    err
+                ),
+            )
+        })?;
+
+    if proto == "http" {
+        let mut parts = req.uri().clone().into_parts();
+        parts.scheme = Some(Scheme::HTTPS);
+        // Read from `Host` header since the incoming request's authority is empty.
+        parts.authority = Some(Authority::try_from(&hostname[..]).map_err(|err| {
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Could not parse hostname {}: {:?}", hostname, err),
+            )
+        })?);
+
+        let uri: String = Uri::from_parts(parts)
+            .map_err(|err| {
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!(
+                        "Could not construct new URI from {:?}: {:?}",
+                        req.uri(),
+                        err
+                    ),
+                )
+            })?
+            .to_string();
+
+        Ok((StatusCode::MOVED_PERMANENTLY, [(header::LOCATION, uri)]).into_response())
+    } else {
+        Ok(next.run(req).await)
+    }
 }
 
 // Music shortlink.
@@ -108,7 +232,10 @@ fn music_shortlink_router(music_shortlink: String) -> Router {
 // All shortlinks.
 fn shortlinks_router(site_url: String, shortlinks: HashMap<String, String>) -> Router {
     Router::new()
-        .route("/", get(|| async move { Redirect::temporary(&site_url) }))
+        .route(
+            "/",
+            get(|| async move { (StatusCode::FOUND, [(header::LOCATION, site_url)]) }),
+        )
         .route(
             "/*path",
             get(|Path((_, shortlink)): Path<(String, String)>| async move {
