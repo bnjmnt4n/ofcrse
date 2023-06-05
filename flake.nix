f9312f36d6de516bb27f717ead92d40d9d991464
Benjamin Tan <benjamin@dev.ofcr.se>
2023-06-05 10:20:05 +0800

    Bump Nix flake

--- flake.nix
+++ flake.nix
@@ -1,62 +1,60 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?rev=04f574a1c0fde90b51bf68198e2297ca4e7cccf4";
-    flake-utils.url = "github:numtide/flake-utils?rev=5aed5285a952e0b949eb3ba02c12fa4fcfef535f";
-    naersk.url = "github:nmattia/naersk";
-    mozillapkgs = {
-      url = "github:mozilla/nixpkgs-mozilla";
-      flake = false;
+    nixpkgs.url = "github:NixOS/nixpkgs?rev=5e871d8aa6f57cc8e0dc087d1c5013f6e212b4ce";
+    flake-utils.url = "github:numtide/flake-utils?rev=cfacdce06f30d2b68473a46042957675eebb3401";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, naersk, mozillapkgs }:
+  outputs = { self, nixpkgs, flake-utils, crane, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages."${system}";
-
-        mozilla = pkgs.callPackage (mozillapkgs + "/package-set.nix") { };
-        rust-channel = mozilla.rustChannelOf {
-          channel = "1.66.0";
-          sha256 = "sha256-S7epLlflwt0d1GZP44u5Xosgf6dRrmr8xxC+Ml2Pq7c=";
-        };
-        rust = rust-channel.rust;
-        rust-src = rust-channel.rust-src;
-
-        naersk-lib = naersk.lib."${system}".override {
-          cargo = rust;
-          rustc = rust;
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
         };
 
-        nativeBuildInputs = [
-          pkgs.openssl
-          pkgs.pkg-config
-        ];
+        rust = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "rustfmt" ];
+        };
+
+        craneLib = (crane.mkLib pkgs).overrideToolchain rust;
+
+        nativeBuildInputs = [ pkgs.openssl pkgs.pkg-config ];
         buildInputs = [ ];
-      in
-      rec {
-        packages.package = naersk-lib.buildPackage {
-          pname = "ofcr.se";
-          root = ./.;
+
+        ofcrse = craneLib.buildPackage {
+          src = craneLib.cleanCargoSource ./.;
+
           inherit nativeBuildInputs buildInputs;
         };
-        defaultPackage = packages.package;
-
-        apps.package = flake-utils.lib.mkApp {
-          drv = packages.package;
+      in
+      rec {
+        # TODO: build Astro app.
+        packages.default = ofcrse;
+        apps.default = flake-utils.lib.mkApp {
+          drv = ofcrse;
         };
-        defaultApp = apps.package;
 
         devShell = pkgs.mkShell {
-          inherit nativeBuildInputs;
+          inputsFrom = [ ofcrse ];
+
           buildInputs = [
             pkgs.flyctl
             pkgs.nodejs-18_x
-
             rust
             pkgs.rust-analyzer
-            pkgs.rustfmt
-          ] ++ buildInputs;
-          RUST_SRC_PATH = "${rust-src}/lib/rustlib/src/rust/library";
+          ];
+
           RUST_LOG = "info";
           RUST_BACKTRACE = 1;
           LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
