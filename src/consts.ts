12cd87ed97b9fc5cbebebd02d89c3d10bad8e257
Benjamin Tan <benjamin@dev.ofcr.se>
2023-06-08 14:24:30 +0800

    Add base components

---
+++ src/consts.ts
@@ -0,0 +1,3 @@
+export const CANONICAL_SITE_URL = "https://ofcr.se/";
+export const SITE_TITLE = "ofcrse by Benjamin tan";
+export const SITE_DESCRIPTION = "ofcrse is Benjamin Tan’s home on the internet.";
