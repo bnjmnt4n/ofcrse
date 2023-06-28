08e777947e5d2c4259b6e0d67bb1348ba706cba7
Benjamin Tan <benjamin@dev.ofcr.se>
2023-06-29 01:49:28 +0800

    Tweak article and writing pages

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
 export const CANONICAL_SITE_URL = "https://ofcr.se/";
 export const SITE_TITLE = "ofcrse by Benjamin Tan";
-export const SITE_DESCRIPTION = "ofcrse is Benjamin Tan’s home on the internet.";
+export const SITE_DESCRIPTION = "ofcrse is the website of Benjamin Tan, a software engineer from Singapore.";
